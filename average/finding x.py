de7ac915b5383609952863138ec30dbde8f137d5
Dasaradh <dasarathram143@gmail.com>
2023-05-03 12:19:44 +0530

    finding numbers

--- average/finding x.py
+++ average/finding x.py
@@ -1,10 +1,10 @@
-a = int(input('total count of numbers = '))
-b = float(input('each number increased/decreased by = '))
-average = float(input('average of numbers= '))
+total_count = int(input('total count of numbers = '))
+value = float(input('each number increased/decreased by = '))
+average = float(input('average of total numbers= '))
 total = 0
-for i in range(a):
-    total += b * i
-x = (a * average - total) / a
+for i in range(total_count):
+    total += value * i
+x = (total_count * average - total) / total_count
 print('the numbers are: ')
-for i in range(a):
-    print(str(x + (b * i)), end=' ')
+for i in range(total_count):
+    print(str(x + (value * i)), end=' ')
