026577eab98e233bf1e4e6e227b89eef8f85e55a
Dasaradh <dasarathram143@gmail.com>
2023-05-03 22:37:18 +0530

    ages qno 18

--- ages/ages.py
+++ ages/ages.py
@@ -13,11 +13,24 @@ def fun1():                                          # page no 267 Qno 35
     q = b * x + d
     print(f'ratio of their ages after {d} years = '+str(p / q))
 
-def fun2():
-    pass
+def fun2():                                         # page no 266 Qno 18
+    a = int(input('enter ratio of a age = '))
+    b = int(input('enter ratio b age = '))
+    h = int(input('enter hence years = '))
+    c = int(input('enter ratio of a age after hence years = '))
+    d = int(input('enter ratio of b age after hence years = '))
+    p= (a*h*c-a*h*d)/(a*d - b*c)
+    q= (b*h*c - b*h*d)/(a*d - b*c)
+    print(p-q)
+
+
+def fun3():
+
 
 a=input('enter option 1 2 3 = ')
 if a=='1':
     fun1()
 elif a=='2':
     fun2()
+else:
+    fun3()
