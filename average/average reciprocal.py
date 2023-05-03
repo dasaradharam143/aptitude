d7887a2f89b9914772eb16dbc5d74fb781b65f82
Dasaradh <dasarathram143@gmail.com>
2023-05-03 10:28:40 +0530

    commit

--- average/average reciprocal.py
+++ average/average reciprocal.py
@@ -1,7 +1,7 @@
-a = list(map(float, input('enter the numbers = ').split()))
+numbers = list(map(float, input('enter the numbers = ').split()))
 b = []
-for i in a:
-    b.append(round(1 / i,5))
+for i in numbers:
+    b.append(round(1 / i, 5))
+print(b)
 avg = sum(b)/len(b)
-print('average = '+str(avg))
-
+print('average = '+str(sum(b))+'/'+str(len(b))+' = '+str(avg))
